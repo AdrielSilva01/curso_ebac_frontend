47e1e7c7d8ae700fa498c1457095199697491988
AdrielSilva01 <131729254+AdrielSilva01@users.noreply.github.com>
2024-02-06 22:22:28 -0300

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,13 @@
+
+function multiplicacao(x: number, y: number): number {
+    return x * y;
+}
+
+
+function saudacao(nome: string): string {
+    return "Olá " + nome;
+}
+
+
+console.log(multiplicacao(5, 3));
+console.log(saudacao("Adriel"));
